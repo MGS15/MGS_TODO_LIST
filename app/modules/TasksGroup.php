d898414e32ac116d17dd958e6ab044aaa74fe241
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-02-04 15:02:20 +0100

    TaksGroup not tasks

--- app/modules/TasksGroup.php
+++ app/modules/TasksGroup.php
@@ -5,7 +5,7 @@ class Task
  * @author          Soufiane Elkhamlichi <selkhamlichi97@gmail.com>
  * @version         1.0
  * @date            2022-02-04
- * @name            Task.php
+ * @name            TasksGroup.php
  */
 	private string	$id;
 	private string	$name;
